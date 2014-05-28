df8bb967654ae7852b01e6ed0e0ee9b63adabe44
benpeloquin7 <benjamin.peloquin@gmail.com>
2014-05-28 08:35:52 -0700

    Initial Commit

---
+++ binary/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  binary
+//
+//  Created by Ben Peloquin on 5/28/14.
+//  Copyright (c) 2014 B. Peloquin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
