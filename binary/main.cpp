3e1089ab023b53999829c01ceb059b324d089871
benpeloquin7 <benjamin.peloquin@gmail.com>
2014-06-02 19:05:36 -0700

    updated main.cpp test file

--- binary/main.cpp
+++ binary/main.cpp
@@ -14,6 +14,47 @@ using namespace std;
 int main() {
 
     Tree<int> bTree(8);
+    bTree.add(12);
+    bTree.add(13);
+    bTree.add(213);
+    bTree.add(312);
+    bTree.add(1);
+    
+    /*
+    cout << "Pre-Order: ";
+    bTree.pre_order();
+    
+    cout << "\nPost-Order: ";
+    bTree.post_order();
+    
+    cout << "\nIn_Order: ";
+    bTree.in_order();
+    */
+    cout << "\nBalancing tree..." << endl;
+    bTree.balance();
+    
+    Tree<int> cTree(bTree);
+    /*
+    cout << "Printing vector...";
+    bTree.printVec();
+    
+    cout << "\nBalanced Pre-Order: ";
+    bTree.pre_order();
+    
+    cout << "\nBalanced Post-Order: ";
+    bTree.post_order();
+    
+    cout << "\nBalanced In_Order: ";
+    bTree.in_order();
+    */
+    
+    cout << "\nPrinting bTree in_order: ";
+    bTree.in_order();
+    
+    cout << "\nPrinting cTree in-order: ";
+    cTree.in_order();
+    
+     
     return 0;
 }
 
